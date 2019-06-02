b5b68a283f0e1ab7ccd0fcaa747c5d535c9a2626
José María Mateos <chema@rinzewind.org>
2019-06-02 19:12:50 -0400

    Empecé a hacer algo pero es posible que los datos del Ministerio no sean fiables. En el notebook hay un ejemplo. Ya les he escrito (veremos)

--- common_functions.py
+++ common_functions.py
@@ -1,127 +1,76 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import csv
-import codecs
-import cStringIO
-import os
-from collections import namedtuple
+import zipfile
+import pandas as pd
 
-def getParties(parties_file):
+def get_parties(parties_file_handler):
     """
     Return the parties along with their numerical code as a
     hashtable (key => code, value => party)
     """
-    f = open(parties_file)
 
     res = dict()
-    for line in f:
+    for line in parties_file_handler:
         # Assign fields of fixed length according to the documentation.
-        code = line[8:14]
-        party = unicode(line[64:214].strip(), "iso-8859-1")
+        code = line[8:14].decode("ascii") # numeric
+        party = line[64:214].strip().decode("iso-8859-1")
         res[code] = party
 
-    f.close()
     return(res)
 
-def getTowns(towns_file):
+def get_election_results(election_file_handler, parties_dict, 
+                         filter_prov_code = '28', 
+                         filter_town_code = '079'):
     """
-    Return a dictionary with the codes for each town along with their names (key => code, value => town).
-    The code is a tuple containing (Province, Town) codes.
+    Return elections results at the level of individual urn. Returns a Pandas 
+    DataFrame with the relevant fields.
+
+    This function doesn't use the information from INE which translates province 
+    and town codes to actual names. We are just interested in Madrid.
+
+    See http://www.ine.es/daco/daco42/codmun/codmun11/11codmunmapa.htm for other codes.
     """
-
-    f = open(towns_file)
-    reader = csv.reader(f)
-    reader.next() # Skip first line, header
-    res = dict()
-
-    for entry in reader: # Skip first line, header
-        code = (entry[0], entry[1]) # (Province, Town)
-        town = unicode(entry[3], "utf-8")
-        if code not in res:
-            res[code] = town
-
-    f.close()
-    return(res)
-
-def getElectionResults(election_file, parties_dict, towns_dict):
-    """
-    Return elections results at the level of individual urn. Return a list of
-    namedtuples with the following fields (see code -- first line).
-    """
-    ElectionEntry = namedtuple('ElectionEntry', 'prov_code, town_code,' + \
-                               'dist_code, section_code,' + \
-                               'table_code, party_code, ' + \
-                               'town_name, party_name, votes')
-    f = open(election_file)
-    def parseLine(line):
+    def parse_line(line):
         # As in getParties(), parse according to official field description.
-        prov_code = line[11:13]
-        town_code = line[13:16]
-        dist_code = line[16:18]
-        section_code = line[18:22]
-        table_code = line[22:23]
-        party_code = line[23:29]
-        votes = line[29:36]
-        
-        entry = ElectionEntry(prov_code, town_code, dist_code, \
-                              section_code, table_code, \
-                              party_code, towns_dict[(prov_code, town_code)], \
-                              parties_dict[party_code], votes)
-        return(entry)
+        prov_code = line[11:13].decode("ascii")
+        town_code = line[13:16].decode("ascii")
 
-    res = map(parseLine, f)
-    return(res)
+        if prov_code == filter_prov_code and town_code == filter_town_code:
+            dist_code = line[16:18].decode("ascii")
+            section_code = line[18:21].decode("ascii")
+            table_code = line[22:23].decode("ascii")
+            party_code = line[23:29].decode("ascii")
+            votes = int(line[29:36].decode("ascii"))
+            
+            entry = dict(prov_code = prov_code, town_code = town_code, 
+                         dist_code = dist_code, section_code = section_code, 
+                         table_code = table_code, party_code = party_code, 
+                         party_name = parties_dict[party_code], 
+                         votes = votes)
+            return(entry)
 
-# From https://docs.python.org/2/library/csv.html#examples
-class UnicodeWriter:
+    res = [parse_line(x) for x in election_file_handler]
+    res = [x for x in res if x]
+
+    return(pd.DataFrame(res))
+
+
+def process_election(zip_file_path, filter_prov_code = '28', filter_town_code = '079'):
     """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
+    Processes the full election data contained in `zip_file_path`.
     """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
-
-#### Main ####
-if (__name__ == "__main__"):
-    # Hardcoded file paths. Change it to use your own data. See README.md 
-    # for info on each file.
-    parties_file = "/home/chema/Dropbox/data/elecciones20112015/04201105_MESA/03041105.DAT"
-    elections_file = "/home/chema/Dropbox/data/elecciones20112015/04201105_MESA/10041105.DAT"
-    towns_file = "/home/chema/Dropbox/data/elecciones20112015/11codmun.csv"
-    parties = getParties(parties_file)
-    towns = getTowns(towns_file)
-    results = getElectionResults(elections_file, parties, towns)
-    # Don't want parties with 0 votes.
-    #results = [x for x in results if int(x.votes) > 0]
-
-    # Save as CSV for analysis with R.
-    if not os.path.isdir('data'):
-        os.mkdir('data')
-
-    f = open('data/elections2011.csv', 'w')
-    csv_writer = UnicodeWriter(f)
-    csv_writer.writerow(results[0]._fields)
-    csv_writer.writerows(results)
-    f.close()
+    with zipfile.ZipFile(zip_file_path, "r") as zfile:
+        listfiles = zfile.namelist()
+        for lf in listfiles:
+            # Do it this way because some times the .zip file contains nested folders.
+            basename = lf.split("/")[-1]
+            # Parties file
+            if basename.startswith("03"):
+                with zfile.open(lf, "r") as fhandler:
+                    parties_dict = get_parties(fhandler)
+            # Results file        
+            if basename.startswith("10"):
+                with zfile.open(lf, "r") as fhandler:
+                    res = get_election_results(fhandler, parties_dict, 
+                                               filter_prov_code, filter_town_code)
+                    
+    return res
